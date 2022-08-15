81b5490ab22105ef9f8156729416db421a6c14fe
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-08-15 18:49:39 +0300

    configuring App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+// import { ToastContainer, toast } from 'toasts-library-svs';
 
 function App() {
-	return <div></div>;
+	const handleAddToast = () => {
+		// toast.setContainer();
+		// toast.generateToast('info', 'medium', 'Info Toast Example');
+	};
+
+	return (
+		<div>
+			<button onClick={handleAddToast}>Add Toast</button>
+			{/* <ToastContainer
+				toastPosition="top-right"
+				toastOffsetX="0"
+				toastOffsetY="0"
+			/> */}
+		</div>
+	);
 }
 
 export default App;
